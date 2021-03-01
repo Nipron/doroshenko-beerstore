a36039417eb1cf0699d2738bd2e097cb1a2df9ca
Vitalii Doroshenko <2328221@ukr.net>
2021-03-01 14:24:42 +0200

    fff

---
+++ src/data.js
@@ -0,0 +1,106 @@
+export const storeProducts = [
+    {
+        id: 1,
+        title: "Pilsner Urquell",
+        img: "img/pivas-01.png",
+        price: 39.90,
+        company: "Google",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, quasi!",
+        inCart: false,
+        count: 0,
+        total: 0
+    },{
+        id: 2,
+        title: "Beck's",
+        img: "img/pivas-02.png",
+        price: 13.00,
+        company: "ПБК Радомышль",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, quasi!",
+        inCart: false,
+        count: 0,
+        total: 0
+    },{
+        id: 3,
+        title: "Spaten",
+        img: "img/pivas-03.png",
+        price: 42.00,
+        company: "Google",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, quasi!",
+        inCart: false,
+        count: 0,
+        total: 0
+    },{
+        id: 4,
+        title: "Heineken",
+        img: "img/pivas-04.png",
+        price: 22.50,
+        company: "Google",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, quasi!",
+        inCart: false,
+        count: 0,
+        total: 0
+    },{
+        id: 5,
+        title: "Grolsch",
+        img: "img/pivas-05.png",
+        price: 50,
+        company: "Google",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, quasi!",
+        inCart: false,
+        count: 0,
+        total: 0
+    },{
+        id: 6,
+        title: "Stiegl",
+        img: "img/pivas-06.png",
+        price: 28,
+        company: "Google",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, quasi!",
+        inCart: false,
+        count: 0,
+        total: 0
+    },{
+        id: 7,
+        title: "Paulaner",
+        img: "img/pivas-07.png",
+        price: 38,
+        company: "Google",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, quasi!",
+        inCart: false,
+        count: 0,
+        total: 0
+    },{
+        id: 8,
+        title: "Tuborg",
+        img: "img/pivas-08.png",
+        price: 17.90,
+        company: "Google",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, quasi!",
+        inCart: false,
+        count: 0,
+        total: 0
+    },{
+        id: 9,
+        title: "Hoegarden",
+        img: "img/pivas-09.png",
+        price: 28.00,
+        company: "Google",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, quasi!",
+        inCart: false,
+        count: 0,
+        total: 0
+    }
+];
+
+export const detailProduct =
+    {
+        id: 1,
+        title: "Pivas",
+        img: "img/pivas-01.png",
+        price: 50,
+        company: "Googgfgfgfgle",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, quasi!",
+        inCart: false,
+        count: 0,
+        total: 0
+    };
